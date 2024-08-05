238ed044b72279bc5574884f73834e145996e25b
abuki113 <abubeker113@gmail.com>
2024-08-05 06:21:35 -0400

    Spider correctly scrape the data from the url except the images

--- ecommerce_scraper/spiders/ecommerce_spider.py
+++ ecommerce_scraper/spiders/ecommerce_spider.py
@@ -1,52 +1,76 @@
 import scrapy
 from scrapy.selector import Selector
 from scrapy_playwright.page import PageMethod
+from ..items import EcommerceScraperItem
 import asyncio
 
 class EcommerceSpiderSpider(scrapy.Spider):
     name = "ecommerce_spider"
-    url = "https://jiji.com.et/cars?filter_attr_100_condition=Brand%20New"
-    def start_requests(self):
-       yield scrapy.Request(url=self.url,
-                            callback=self.parse,
-                            meta=dict(
-                                playwright=True,
-                                playwright_include_page=True,
-                                playwright_page_methods=[
-                                    PageMethod("wait_for_timeout", 10),
-                                    ],
-                                errback=self.errback,
-                            )
-                             )
+    start_urls = [
+        "https://jiji.com.et/cars?filter_attr_100_condition=Brand%20New"
+    ]
 
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(
+                url=url,
+                callback=self.parse,
+                meta={
+                    'playwright': True,
+                    'playwright_include_page': True,
+                    'playwright_page_methods': [
+                        PageMethod("wait_for_timeout", 10),
+                    ],
+                    'errback': self.errback,
+                    
+                },
+            )
 
     async def parse(self, response):
         page = response.meta["playwright_page"]
-
-        await asyncio.sleep(10)
-        html_str = await page.content()            
+        # await asyncio.sleep(10)
+        html_str = await page.content()
         await page.close()
+
         content = Selector(text=html_str)
         products = content.css("div.masonry-item")
+
         for product in products:
-            yield {
-                'name': product.css("div.b-advert-title-inner.qa-advert-title.b-advert-title-inner--div::text").get(),
-                'price': product.css("div.qa-advert-price::text").get(),
-            }
-        next_page = response.css('a.pagination__next::attr(href)').get()
-        if next_page:
-            next_page_url  = "https://jiji.com.et" + next_page
-            yield response.follow(url=next_page_url, 
-                                  callback=self.parse,
-                                  meta=dict(
-                                    playwright=True,
-                                    playwright_include_page=True,
-                                    playwright_page_methods=[
-                                        PageMethod("wait_for_timeout", 10),
-                                        ],
-                                    errback=self.errback,
-                            ))
-    
+            product_url = "https://jiji.com.et" +  str(product.css("a").attrib['href'])
+            yield scrapy.Request(
+                url=product_url,
+                callback=self.parse_product_data,
+            )
+        # next_page = response.css('a.pagination__next::attr(href)').get()
+        # if next_page:
+        #     next_page_url = "https://jiji.com.et" + next_page
+        #     yield response.follow(
+        #         url=next_page_url,
+        #         callback=self.parse,
+        #         meta={
+        #             'playwright': True,
+        #             'playwright_include_page': True,
+        #             'playwright_page_methods': [
+        #                 PageMethod("wait_for_timeout", 10),
+        #             ],
+        #             'errback': self.errback,
+        #         }
+        #     )
+
+    async def parse_product_data(self, response):
+        # page = response.meta["playwright_page"]
+        # # await asyncio.sleep(10)
+        # html_str = await page.content()
+        # await page.close()
+
+        content = response
+
+        prod_item = EcommerceScraperItem()
+        prod_item["name"] = content.css("div.b-advert-title-inner.qa-advert-title.b-advert-title-inner--h1::text").get()
+        prod_item["description"] = [ for cxs in content.css("div.b-advert-attributes__tag::text").getall()]
+
+        yield prod_item
+
     async def errback(self, failure):
         page = failure.request.meta["playwright"]
         await page.close()
